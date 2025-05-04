3dc706dfcba37f93646559cb1c451d06072cd87a
Mohammed Atef <mohammed.atef.4200@gmail.com>
2025-05-04 15:31:40 +0300

    update: max width wrapper component

--- src/components/MaxWidthWrapper.tsx
+++ src/components/MaxWidthWrapper.tsx
@@ -1,13 +1,13 @@
 import { cn } from "@/lib/utils"
 
 interface MaxWidthWrapperProps {
-  className: string;
+  className?: string;
   children: React.ReactNode;
 }
 
 const MaxWidthWrapper = ({className, children}: MaxWidthWrapperProps) => {
   return (
-    <div className={cn("h-auto mx-auto max-w-screen-xl px-2.5 md:px-20", className)}>{children}</div>
+    <div className={cn("h-auto mx-auto w-full max-w-screen-xl px-2.5 md:px-20", className)}>{children}</div>
   )
 }
 
