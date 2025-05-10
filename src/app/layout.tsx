74ece0d6109ca76a06b7bd0da362353930d4902b
Mohammed Atef <mohammed.atef.4200@gmail.com>
2025-05-10 10:14:11 +0300

    change: font familly

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,15 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import { Toaster } from "@/components/ui/sonner"
 import Footer from "@/components/Footer";
+import { Recursive } from 'next/font/google'
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const recursive = Recursive({
+  subsets: ['latin'],
+  variable: '--font-recursive',
+})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -28,7 +24,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${recursive.variable} antialiased`}
       >
         <main className="flex flex-col min-h-screen">
           <Navbar />
