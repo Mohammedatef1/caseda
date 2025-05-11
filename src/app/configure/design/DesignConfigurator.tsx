9848d2dbed0473bf0802b056b7a4e62e4d29ca39
Mohammed Atef <mohammed.atef.4200@gmail.com>
2025-05-11 11:19:24 +0300

    add: colors options

--- src/app/configure/design/DesignConfigurator.tsx
+++ src/app/configure/design/DesignConfigurator.tsx
@@ -1,8 +1,12 @@
 "use client"
 
+import { COLORS } from '@/app/validators/option-validator';
 import HandleComponent from '@/components/HandleComponent';
+import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group';
+import { ScrollArea } from '@/components/ui/scroll-area';
 import { cn } from '@/lib/utils';
 import Image from 'next/image';
+import { useState } from 'react';
 import { Rnd } from 'react-rnd'
 
 interface DesignConfiguratorProps {
@@ -12,6 +16,10 @@ interface DesignConfiguratorProps {
 }
 
 const DesignConfigurator = ({configId, imageDimensions, imgUrl} : DesignConfiguratorProps) => {
+  const [options, setOptions] = useState({
+    color: COLORS[0]
+  })
+
   return (
     <div className="relative grid grid-cols-1 md:grid-cols-3 overflow-hidden my-10">
       <div className="relative h-[37.5rem] md:col-span-2 border border-dashed border-gray-300 overflow-hidden rounded-2xl flex items-center justify-center p-4 md:p-6 ">
@@ -21,7 +29,7 @@ const DesignConfigurator = ({configId, imageDimensions, imgUrl} : DesignConfigur
           <div
             className={cn(
               'absolute inset-0 left-[3px] top-px right-[3px] bottom-px rounded-[32px] pointer-events-none',
-              `bg-blue-500`
+              `bg-${options.color.tw}`
             )}
           />
         </div>
@@ -36,7 +44,22 @@ const DesignConfigurator = ({configId, imageDimensions, imgUrl} : DesignConfigur
           <Image fill src={imgUrl} alt='' className='select-none pointer-events-none' />
         </Rnd>
       </div>
-      <div className="md:col-span-1"></div>
+      <div className="md:col-span-1 p-4 sm:p-6 h-[37.5rem]">
+        <h3 className='font-bold text-2xl md:text-4xl mb-8'>Customize your case.</h3>
+        <ScrollArea className='flex-1 h-full'>
+          <span>color: {options.color.label}</span>
+          <RadioGroup onChange={(e) => console.log(e)}  className='flex items-center gap-x-2 flex-wrap mt-2'>
+            {COLORS.map(item => (
+              <div className={`p-0.5 border rounded-full transition-colors ${options.color === item ? `border-${item.tw}` : 'border-transparent'}`} key={item.value}>
+                <RadioGroupItem value={item.value} id={`color-${item.value}`} className='hidden' />
+                <label onClick={() => {setOptions(prev => {
+                  return {...prev, color: item}
+                })}} htmlFor={`color-${item.value}`} className={`w-8 h-8 rounded-full block bg-${item.tw}`} ></label>
+              </div>
+            ))}
+          </RadioGroup>
+        </ScrollArea>
+      </div>
     </div>
   )
 }
---
+++ src/app/validators/option-validator.tsx
@@ -0,0 +1,21 @@
+export const COLORS = [
+  {
+  label: 'Black',
+  value: 'black',
+  tw: 'zinc-950'
+  },
+  {
+  label: 'Blue',
+  value: 'blue',
+  tw: 'blue-900'
+  },
+  {
+  label: 'Red',
+  value: 'red',
+  tw: 'red-900'
+  }
+] 
+
+// bg-blue-900 border-blue-900
+// bg-red-900 border-red-900
+// bg-zinc-950 border-zinc-950
