83965487d806f70bafd86d0145a0d63a823d07b2
Mohammed Atef <mohammed.atef.4200@gmail.com>
2025-05-13 00:08:45 +0300

    add: crop the image functionality

--- src/app/configure/design/DesignConfigurator.tsx
+++ src/app/configure/design/DesignConfigurator.tsx
@@ -5,8 +5,8 @@ import HandleComponent from '@/components/HandleComponent';
 import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group';
 import { ScrollArea } from '@/components/ui/scroll-area';
 import { cn } from '@/lib/utils';
-import Image from 'next/image';
-import { useState } from 'react';
+import NextImage from 'next/image';
+import { useRef, useState } from 'react';
 import { Rnd } from 'react-rnd'
 import {
   Select,
@@ -17,6 +17,8 @@ import {
 } from "@/components/ui/select"
 import { Button } from '@/components/ui/button';
 import { MoveRight } from 'lucide-react';
+import { toast } from 'sonner';
+import { useUploadThing } from '@/lib/uploadThing';
 
 interface DesignConfiguratorProps {
   configId: string;
@@ -25,15 +27,91 @@ interface DesignConfiguratorProps {
 }
 
 const DesignConfigurator = ({configId, imageDimensions, imgUrl} : DesignConfiguratorProps) => {
+
+  const {startUpload, isUploading} = useUploadThing("imageUploader")
+
+  const containerRef = useRef<HTMLDivElement>(null)
+  const caseRef = useRef<HTMLDivElement>(null)
+
   const [options, setOptions] = useState({
     color: COLORS.options[0],
     model: MODELS.options[0].value
   })
+  const [renderedImagePosition, setRenderedImagePosition] = useState({
+    x: 150,
+    y: 150
+  })
+
+  const [renderedImageDimensions, setRenderedImageDimensions] = useState({
+    width: imageDimensions.width,
+    height: imageDimensions.height
+  })
+
+  const getCroppedImage = async() => {
+    try {
+      const containerRect = containerRef.current?.getBoundingClientRect()
+      const caseRect = caseRef.current?.getBoundingClientRect()
+
+      if (!caseRect || !containerRect) return
+
+      const caseRelativeToContainer = {
+        x: caseRect?.x - containerRect?.x,
+        y : caseRect?.y - containerRect?.y 
+      }
+
+      const imageRelativeToCase = {
+        x: renderedImagePosition.x - caseRelativeToContainer.x,
+        y: renderedImagePosition.y - caseRelativeToContainer.y
+      }
+      
+      const canvas = document.createElement('canvas');
+      canvas.width = caseRect.width
+      canvas.height = caseRect.height
+      const ctx = canvas.getContext('2d')
+
+      const userImage = new Image()
+      userImage.crossOrigin = 'anonymous'
+      userImage.src = imgUrl
+      await new Promise(resolve => userImage.onload = resolve)
+
+      ctx?.drawImage(
+        userImage,
+        imageRelativeToCase.x,
+        imageRelativeToCase.y,
+        renderedImageDimensions.width,
+        renderedImageDimensions.height
+      )
+
+      const base64 = ctx?.canvas.toDataURL()
+      const base64Data = base64?.split(',')[1]
+
+      const blob = convert64ToBlob(base64Data!, 'image/png')
+      const file = new File([blob], `case-${configId.slice(0, 4)}`, {type: 'image/png'} )
+
+      await startUpload([file], {configId})
+    } catch (error) {
+      console.log(error)
+      toast.error('something went wrong!')
+    } 
+  }
+
+  const convert64ToBlob = (data: string, type: string) => {
+    const byteCharacters = atob(data) // decode from 64 to characters
+    const byteNumbs = [] 
+    for(let i = 0; i < byteCharacters.length; i++ ) {
+      byteNumbs[i] = byteCharacters.charCodeAt(i) // return the unicode of each character (0 : 255)
+    }
+
+    const byteArray = new Uint8Array(byteNumbs) // just convert the array to unit8array to be accepted by Blob
+    const blob = new Blob([byteArray], { type })
+
+    return blob
+  }
 
   return (
     <div className="relative grid grid-cols-1 md:grid-cols-3 overflow-hidden my-10">
-      <div className="relative h-[37.5rem] md:col-span-2 border border-dashed border-gray-300 overflow-hidden rounded-2xl flex items-center justify-center p-4 md:p-6 ">
-        <div className="relative h-full max-h-full rounded-[45px]">
+      <div ref={containerRef} className="relative h-[37.5rem] md:col-span-2 border border-dashed border-gray-300 overflow-hidden rounded-2xl flex items-center justify-center p-4 md:p-6 ">
+        <div  ref={caseRef} className="relative h-full max-h-full rounded-[45px]">
           <img src="/phone-template.png" alt="" className="w-full h-full object-contain relative z-50 pointer-events-none select-none" />
           <div className='absolute z-40 inset-0 left-[3px] top-px right-[3px] bottom-px rounded-[32px] shadow-[0_0_0_99999px_rgba(229,231,235,0.6)] pointer-events-none' />
           <div
@@ -43,7 +121,13 @@ const DesignConfigurator = ({configId, imageDimensions, imgUrl} : DesignConfigur
             )}
           />
         </div>
-        <Rnd default={{ x:150,y: 150, width: imageDimensions.width, height: imageDimensions.height  }} lockAspectRatio 
+        <Rnd
+        onDragStop={(_,{x, y}) => setRenderedImagePosition({x, y})}
+        onResizeStop={(_, __, element, ___, {x, y}) => {
+          setRenderedImagePosition({x, y})
+          setRenderedImageDimensions({width : parseInt(element.style.width.slice(0, -2)), height: parseInt(element.style.height.slice(0, -2))})
+        }}
+        default={{ x:150,y: 150, width: imageDimensions.width, height: imageDimensions.height  }} lockAspectRatio 
         className='absolute z-20'
         resizeHandleComponent={{
           topLeft: <HandleComponent/>,
@@ -51,7 +135,7 @@ const DesignConfigurator = ({configId, imageDimensions, imgUrl} : DesignConfigur
           bottomLeft: <HandleComponent/>,
           bottomRight: <HandleComponent/>
         }}>
-          <Image fill src={imgUrl} alt='' className='select-none pointer-events-none' />
+          <NextImage fill src={imgUrl} alt='' className='select-none pointer-events-none' />
         </Rnd>
       </div>
       <div className="md:col-span-1 p-4 sm:p-6 h-[37.5rem] flex flex-col gap-y-5">
@@ -60,7 +144,7 @@ const DesignConfigurator = ({configId, imageDimensions, imgUrl} : DesignConfigur
           <div className='flex flex-col gap-y-5'>
             <div>
               <span>{COLORS.label}: {options.color.label}</span>
-              <RadioGroup onChange={(e) => console.log(e)}  className='flex items-center gap-x-2 flex-wrap mt-2'>
+              <RadioGroup className='flex items-center gap-x-2 flex-wrap mt-2'>
                 {COLORS.options.map(item => (
                   <div className={`p-0.5 border rounded-full transition-colors ${options.color === item ? `border-${item.tw}` : 'border-transparent'}`} key={item.value}>
                     <RadioGroupItem value={item.value} id={`color-${item.value}`} className='hidden' />
@@ -90,9 +174,9 @@ const DesignConfigurator = ({configId, imageDimensions, imgUrl} : DesignConfigur
             </div>
           </div>
         </ScrollArea>
-        <div className='flex items-center gap-x-2 md:gap-x-5 cursor-pointer'>
+        <div className='flex items-center gap-x-2 md:gap-x-5'>
           <span>$14.00</span>
-          <Button className='flex-1'>
+          <Button onClick={getCroppedImage} disabled={isUploading} className='flex-1 cursor-pointer disabled:opacity-75 transition-opacity'>
             <span>Continue</span>
             <MoveRight />
           </Button>
